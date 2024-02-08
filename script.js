89d66b2d74dc128490cc47862c1bdd7ad340c91b
MahammadUmar  Shaikh <us613560@gmail.com>
2024-02-08 18:10:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+exchageIcon = document.querySelector(".exchange"),
+selectTag = document.querySelectorAll("select"),
+icons = document.querySelectorAll(".row i");
+translateBtn = document.getElementById("button2");
+
+const fileSelector = document.querySelector('input')
+const start = document.getElementById('button1')
+const img = document.querySelector('img')
+const progress = document.querySelector('.progress')
+const textarea = document.querySelector('textarea')
+const text = document.querySelector("Text")
+
+// first show image on upload
+fileSelector.onchange = () => {
+    var file = fileSelector.files[0]
+    var imgUrl = window.URL.createObjectURL(new Blob([file], { type: 'image/jpg' }))
+    img.src = imgUrl
+}
+
+// now start text recognition
+start.onclick = () => {
+    textarea.innerHTML = ''
+    const rec = new Tesseract.TesseractWorker()
+    rec.recognize(fileSelector.files[0])
+        .progress(function (response) {
+            if(response.status == 'recognizing text'){
+                progress.innerHTML = response.status
+            }else{
+                progress.innerHTML = response.status
+            }
+        })
+        .then(function (data) {
+            textarea.innerHTML = data.text
+            progress.innerHTML = 'Done'
+        })
+}
+
+selectTag.forEach((tag, id) => {
+    for (let country_code in countries) {
+        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";
+        let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
+    }
+});
+
+exchageIcon.addEventListener("click", () => {
+    let tempText = fromText.value,
+    tempLang = selectTag[0].value;
+    fromText.value = toText.value;
+    toText.value = tempText;
+    selectTag[0].value = selectTag[1].value;
+    selectTag[1].value = tempLang;
+});
+
+fromText.addEventListener("keyup", () => {
+    if(!fromText.value) {
+        toText.value = "";
+    }
+});
+
+translateBtn.addEventListener("click", () => {
+    let text = fromText.value.trim(),
+    translateFrom = selectTag[0].value,
+    translateTo = selectTag[1].value;
+    if(!text) return;
+    toText.setAttribute("placeholder", "Translating...");
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        toText.value = data.responseData.translatedText;
+        data.matches.forEach(data => {
+            if(data.id === 0) {
+                toText.value = data.translation;
+            }
+        });
+        toText.setAttribute("placeholder", "Translation");
+    });
+});
+
+icons.forEach(icon => {
+    icon.addEventListener("click", ({target}) => {
+        if(!fromText.value || !toText.value) return;
+        if(target.classList.contains("fa-copy")) {
+            if(target.id == "from") {
+                navigator.clipboard.writeText(fromText.value);
+            } else {
+                navigator.clipboard.writeText(toText.value);
+            }
+        } else {
+            let utterance;
+            if(target.id == "from") {
+                utterance = new SpeechSynthesisUtterance(fromText.value);
+                utterance.lang = selectTag[0].value;
+            } else {
+                utterance = new SpeechSynthesisUtterance(toText.value);
+                utterance.lang = selectTag[1].value;
+            }
+            speechSynthesis.speak(utterance);
+        }
+    });
+});
